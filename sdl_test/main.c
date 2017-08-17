88cc5ac6b4e9f5a98aaf0d3319dd576157f32cd0
bplu4t2f <bplu4t2f@gmail.com>
2017-08-17 17:54:44 +0200

    Added a VBO example.

--- sdl_test/main.c
+++ sdl_test/main.c
@@ -1,29 +1,26 @@
 // Need this to bypass the retarded main redirect logic
 #define SDL_MAIN_HANDLED
 
-#define GL_GLEXT_PROTOTYPES
-
 #include <stdio.h>
+#include <GL/glew.h>
 #include <SDL.h>
 #include <Windows.h>
-#include <SDL_opengl.h>
-
 #include <gl/GL.h>
 #include <gl/GLU.h>
+#include <SDL_opengl.h>
 
 
 static int main_opengl();
+// Should use D3D9 by default
 static int main_sdl();
+static int main_vbos();
+
+#define the_main main_opengl
 
 
 int main(int argc, char **argv)
 {
-#if 1
-	main_opengl();
-#else
-	// Should use D3D9 by default
-	main_sdl();
-#endif
+	the_main();
 }
 
 
@@ -54,6 +51,12 @@ int main_opengl()
 	// Enabling multisampling seems to improve the issue, but it's not good enough yet.
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 0); // @TODO doesn't seem to be necessary
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
+#endif
+
+#if 0
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 #endif
 
 	SDL_Window *window = SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
@@ -116,69 +119,8 @@ int main_opengl()
 
 
 
-#if 0
-		// This block is stolen from obbg (https://github.com/nothings/obbg)
-		// See main.c line 600 and following
-		// If we can get something equivalent working, the problem should be alleviated significantly.
-
-		for (distance = 0; distance <= rad; ++distance) {
-			for (j = -distance; j <= distance; ++j) {
-				for (i = -distance; i <= distance; ++i) {
-					int cx = qchunk_x + i;
-					int cy = qchunk_y + j;
-					int slot_x = cx & (C_MESH_CHUNK_CACHE_X - 1);
-					int slot_y = cy & (C_MESH_CHUNK_CACHE_Y - 1);
-					mesh_chunk *mc = c_mesh_cache[slot_y][slot_x];
-
-					if (stb_max(abs(i), abs(j)) != distance)
-						continue;
-
-					if (i*i + j*j > rad*rad)
-						continue;
-
-					if (mc == NULL || mc->placeholder_for_size_info || mc->chunk_x != cx || mc->chunk_y != cy || mc->vbuf == 0) {
-						float estimated_bounds[2][3];
-						if (num_build_remaining == 0)
-							continue;
-						estimated_bounds[0][0] = (float)(cx << MESH_CHUNK_SIZE_X_LOG2);
-						estimated_bounds[0][1] = (float)(cy << MESH_CHUNK_SIZE_Y_LOG2);
-						estimated_bounds[0][2] = (float)(0);
-						estimated_bounds[1][0] = (float)((cx + 1) << MESH_CHUNK_SIZE_X_LOG2);
-						estimated_bounds[1][1] = (float)((cy + 1) << MESH_CHUNK_SIZE_Y_LOG2);
-						estimated_bounds[1][2] = (float)(255);
-						if (!is_box_in_frustum(estimated_bounds[0], estimated_bounds[1]))
-							continue;
-						mc = build_mesh_chunk_for_coord(mc, cx * C_MESH_CHUNK_CACHE_X, cy * C_MESH_CHUNK_CACHE_Y);
-						--num_build_remaining;
-					}
-
-					++chunk_locations;
-
-					++chunks_considered;
-					quads_considered += mc->num_quads;
-					chunk_storage_considered += mc->num_quads * 20;
-
-					if (mc->num_quads && !mc->placeholder_for_size_info) {
-						if (is_box_in_frustum(mc->bounds[0], mc->bounds[1])) {
-							// @TODO if in range, frustum cull
-							stbglUniform3fv(stbgl_find_uniform(main_prog, "transform"), 3, mc->transform[0]);
-							glBindBufferARB(GL_ARRAY_BUFFER_ARB, mc->vbuf);
-							glVertexAttribIPointer(0, 1, GL_UNSIGNED_INT, 4, (void*)0);
-							glBindTexture(GL_TEXTURE_BUFFER_ARB, mc->fbuf_tex);
-
-							glDrawArrays(GL_QUADS, 0, mc->num_quads * 4);
-
-							quads_rendered += mc->num_quads;
-							++chunks_in_frustum;
-							chunk_storage_rendered += mc->num_quads * 20;
-						}
-					}
-				}
-			}
-		}
-#endif
-
 		int fc_mod4 = frame_counter % 4;
+		// "Divide-remainder" by the number of colored rects you want
 		int fc_mod8 = frame_counter % 8;
 
 		if (frame_counter & 1)
@@ -219,9 +161,24 @@ int main_opengl()
 		glEnd();
 #endif
 
-		
+#if 0
+		// Alternative way to fix it (instead of drawing many rects): Call this often
+		for (int i = 0; i < 10000; ++i)
+		{
+			glFlush();
+		}
+#endif
+
 		glFlush();
 		SDL_GL_SwapWindow(window);
+
+#if 0
+		auto errors = glGetError();
+		if (errors != 0)
+		{
+			printf("ERROR %x\r\n", (int)errors);
+		}
+#endif
 	}
 
 _quit:
@@ -268,7 +225,7 @@ int main_sdl()
 #endif
 	}
 
-	Uint32 sdl_renderer_flags = SDL_RENDERER_ACCELERATED;
+	Uint32 sdl_renderer_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE;
 #if 1
 	// enable vsync
 	sdl_renderer_flags |= SDL_RENDERER_PRESENTVSYNC;
@@ -285,6 +242,8 @@ int main_sdl()
 
 	Uint64 performance_frequency = SDL_GetPerformanceFrequency();
 	Uint64 last_tick = SDL_GetPerformanceCounter();
+
+	SDL_Texture *texTarget = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 800, 600);
 
 	while (1)
 	{
@@ -343,6 +302,20 @@ int main_sdl()
 		SDL_RenderFillRects(renderer, rects, RECT_COUNT);
 #endif
 
+		SDL_SetRenderTarget(renderer, texTarget);
+
+		SDL_RenderClear(renderer);
+		SDL_Rect rect1;
+		rect1.x = 500;
+		rect1.y = 100;
+		rect1.w = 40;
+		rect1.h = 30;
+		SDL_RenderFillRect(renderer, &rect1);
+
+		SDL_SetRenderTarget(renderer, NULL);
+
+		SDL_RenderCopy(renderer, texTarget, NULL, &rect1);
+
 		SDL_RenderPresent(renderer);
 	}
 
@@ -352,5 +325,246 @@ _quit:
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
+	return 0;
+}
+
+
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+int main_vbos()
+{
+	// settings
+
+		// glfw: initialize and configure
+		// ------------------------------
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+	SDL_Window *window = SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCR_WIDTH, SCR_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	if (window == NULL)
+	{
+		return -1;
+	}
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+	if (context == NULL)
+	{
+		return 1;
+	}
+
+	glewExperimental = GL_TRUE;
+	GLenum glewError = glewInit();
+	if (glewError != GLEW_OK)
+	{
+		printf("Error initializing GLEW! %s\n", glewGetErrorString(glewError));
+		return 1;
+	}
+
+	if (SDL_GL_MakeCurrent(window, context) != 0)
+	{
+		return 1;
+	}
+
+#if 0
+	// "Enable vsync"
+	if (SDL_GL_SetSwapInterval(1) != 0)
+	{
+		return 1;
+	}
+#endif
+
+	// build and compile our shader program
+	// ------------------------------------
+	// vertex shader
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		return 1;
+		//std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// fragment shader
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		return 1;
+		//std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// link shaders
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		return 1;
+		//std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+#define NUM_TRIANGLES 10000
+#define NUM_VERTICES_PER_TRIANGLE 3
+#define NUM_FLOATS_PER_VERTEX 3
+	float vertices[NUM_TRIANGLES * NUM_VERTICES_PER_TRIANGLE * NUM_FLOATS_PER_VERTEX];
+	for (int i = 0; i < NUM_TRIANGLES; ++i)
+	{
+		int cursor = 0;
+		vertices[i * 9 + cursor++] = 0.5f;
+		vertices[i * 9 + cursor++] = 0.5f;
+		vertices[i * 9 + cursor++] = 0.0f;
+
+		vertices[i * 9 + cursor++] = 0.5f;
+		vertices[i * 9 + cursor++] = -0.5f;
+		vertices[i * 9 + cursor++] = 0.0f;
+
+		vertices[i * 9 + cursor++] = -0.5f;
+		vertices[i * 9 + cursor++] = -0.5f;
+		vertices[i * 9 + cursor++] = 0.0f;
+	}
+
+#if 0
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+#endif
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+#if 0
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+#endif
+
+	glVertexAttribPointer(0, NUM_FLOATS_PER_VERTEX, GL_FLOAT, GL_FALSE, NUM_FLOATS_PER_VERTEX * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+
+	// uncomment this call to draw in wireframe polygons.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	int frame_counter = 0;
+
+	// render loop
+	// -----------
+	while (1)
+	{
+		// input
+		// -----
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				goto _end;
+			}
+		}
+
+		++frame_counter;
+
+		// render
+		// ------
+		if (frame_counter & 1)
+		{
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		}
+		else
+		{
+			glClearColor(0, 0, 0, 1.0f);
+		}
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+#if 1
+		glDrawArrays(GL_TRIANGLES, 0, NUM_TRIANGLES * NUM_VERTICES_PER_TRIANGLE);
+#else
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+#endif
+		// glBindVertexArray(0); // no need to unbind it every time 
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+
+		SDL_GL_SwapWindow(window);
+
+#if 0
+		auto errors = glGetError();
+		if (errors != 0)
+		{
+			printf("ERROR %x\r\n", (int)errors);
+		}
+#endif
+
+		SDL_Delay(10);
+	}
+
+_end:
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
 	return 0;
 }
